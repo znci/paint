4a283668f3ce633fb3eaf742ce50ea3ec10bfc6c
BlockArchitech <hello@blockarchitech.com>
2023-07-07 18:34:14 -0400

    didn't all push?

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,24 @@ router.get("/app", function (req, res, next) {
   res.sendFile("app.html", { root: "./public" });
 });
 
+router.get("/app/collaborate", function (req, res, next) {
+  res.sendFile("collaborate.html", { root: "./public" });
+});
+
+router.post("/app/collaborate", function (req, res, next) {
+  var collab_id = req.body.collab_id;
+  // TODO: check if collab_id is valid
+  res.redirect("/app/collaborate/" + collab_id);
+});
+
+router.get("/app/collaborate/:collab_id", function (req, res, next) {
+  var collab_id = req.params.collab_id;
+  // TODO: collaborative painting
+  res.send(collab_id);
+});
+
+router.get("/favicon.ico", function (req, res, next) {
+  res.sendFile("favicon.ico", { root: "./public" });
+});
+
 module.exports = router;
